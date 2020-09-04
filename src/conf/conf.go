04a1e179686d35b6bf82b51fe5a96c2cbeca1f62
jameshwc <jameshwc@gmail.com>
2020-09-04 15:29:25 +0800

    build the code structure

---
+++ src/conf/conf.go
@@ -0,0 +1,31 @@
+package conf
+
+import (
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Database struct {
+	User     string
+	Password string
+	Host     string
+}
+
+type Server struct {
+	RunMode  string
+	HttpPort int
+}
+
+type Config struct {
+	DB     Database `toml:"database"`
+	Server Server
+}
+
+var Conf = &Config{}
+
+func Setup(configPath string) {
+	if _, err := toml.DecodeFile(configPath, Conf); err != nil {
+		log.Fatalf("read config")
+	}
+}
